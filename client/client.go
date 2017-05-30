82fd984d798c3c0b59e25047d2a4086d0d38c615
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2017-05-30 17:05:47 +0000

    removed client deadlines and no longer disconnects

--- client/client.go
+++ client/client.go
@@ -25,57 +25,127 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+// This example based on https://github.com/gorilla/websocket/blob/master/examples/chat/client.go
+
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "time"
-    
-    "github.com/gorilla/websocket"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"net/http"
 )
 
-func main () {
-    resp, err := http.Get("http://localhost:8080/")
-    if err != nil {
-        panic(err)
-    }
-    
-    defer resp.Body.Close()
-    
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        panic(err)
-    }
-    
-    fmt.Println("get:", string(body))
-    
-    var dialer *websocket.Dialer
-    
-    con, _, err := dialer.Dial("ws://localhost:8080/ws", nil)
-    if err != nil {
-        panic(err)
-    }
-    
-    go func (con *websocket.Conn) {
-        for {
-            _, message, err := con.ReadMessage()
-            if err != nil {
-                panic(err)
-            }
-            
-            fmt.Println("recv:", string(message))
-        }
-    } (con)
-    
-    go func (con *websocket.Conn) {
-        for {
-            time.Sleep(time.Millisecond * 500)
-            con.WriteMessage(websocket.TextMessage, []byte(time.Now().Format("2006-01-02 15:04:05.000")))
-        }
-        
-    } (con)
-    
-    time.Sleep(time.Second * 60)
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 7) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+func main() {
+	resp, err := http.Get("http://localhost:8080/")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("get:", string(body))
+
+	var dialer *websocket.Dialer
+
+	con, _, err := dialer.Dial("ws://localhost:8080/ws", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	wchan := make(chan []byte)
+
+	pchan := make(chan []byte)
+
+	go func(con *websocket.Conn) {
+		defer con.Close()
+		con.SetReadLimit(maxMessageSize)
+		//con.SetReadDeadline(time.Now().Add(pongWait))
+		con.SetReadDeadline(time.Time{})
+		con.SetPongHandler(func(s string) error {
+			fmt.Printf("received PONG from %s\n", con.UnderlyingConn().RemoteAddr().String())
+			//con.SetReadDeadline(time.Now().Add(pongWait))
+			return nil
+		})
+		con.SetPingHandler(func(s string) error {
+			fmt.Printf("received PING (%s) from %s\n", s, con.UnderlyingConn().RemoteAddr().String())
+			//con.SetReadDeadline(time.Now().Add(pongWait))
+			pchan <- []byte(s)
+			return nil
+		})
+		for {
+			_, message, err := con.ReadMessage()
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println("recv:", string(message))
+		}
+	}(con)
+
+	go func(con *websocket.Conn, wchan chan []byte) {
+		pingtimer := time.NewTicker(pingPeriod)
+		defer con.Close()
+		con.SetWriteDeadline(time.Time{})
+		for {
+			select {
+			case wmsg := <-wchan:
+				w, err := con.NextWriter(websocket.TextMessage)
+				if err != nil {
+					return
+				}
+				w.Write(wmsg)
+
+				if err := w.Close(); err != nil {
+					return
+				}
+
+			case pmsg := <-pchan:
+				//con.SetWriteDeadline(time.Now().Add(writeWait))
+				fmt.Printf("sending PONG to %s\n", con.UnderlyingConn().RemoteAddr().String())
+				if err := con.WriteMessage(websocket.PongMessage, pmsg); err != nil {
+					return
+				}
+
+			case <-pingtimer.C:
+				//con.SetWriteDeadline(time.Now().Add(writeWait))
+				fmt.Printf("sending PING to %s\n", con.UnderlyingConn().RemoteAddr().String())
+				if err := con.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+					return
+				}
+			}
+		}
+	}(con, wchan)
+
+	go func(con *websocket.Conn) {
+		time.Sleep(time.Second * 0)
+		for {
+			time.Sleep(time.Millisecond * 500)
+			wchan <- []byte(time.Now().Format("2006-01-02 15:04:05.000"))
+		}
+
+	}(con)
+
+	time.Sleep(time.Second * 600)
 }
